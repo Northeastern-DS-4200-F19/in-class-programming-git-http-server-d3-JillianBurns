b54db24ee24f6e9a85261237104e0a2b01211a79
JillianBurns <jillian.jb.burns@gmail.com>
2019-09-10 14:42:31 -0400

    add svg padding, center bars within barWidth, add dots

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
 var svgWidth = 500,
     svgHeight = 400,
-    barPadding = 10,
-    barWidth = (svgWidth / dataset.length);
+    svgPadding = {x:10, y:10},
+    barPadding = 20,
+    barWidth = (svgWidth-(svgPadding.x*2)) / dataset.length; //width of bar including padding
 
-
-var svg = d3.select('svg')
+var svg = d3.select('.bar-chart')
     .attr("width", svgWidth)
     .attr("height", svgHeight);
     
@@ -18,8 +18,22 @@ var barChart = svg.selectAll("rect")
     .attr("y", d => svgHeight - d) 
     .attr("height", d => d)
     .attr("width", barWidth - barPadding)
-    .style("fill", "red") //enter a color here
+    .style("fill", "red")
+    //.attr("visibility", "hidden")
     .attr("transform", function (d, i) {
-        let translate = [barWidth * i, 0]; 
+        let translate = [svgPadding.x + (barWidth*i) + (barPadding/2), 0]; 
+        return "translate("+ translate +")";
+    });
+
+var barChart = svg.selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle")
+    .attr("cy", d => svgHeight - d) 
+    .attr("r", 5)
+    .style("fill", "white")
+    //.attr("visibility", "hidden")
+    .attr("transform", function (d, i) {
+        let translate = [svgPadding.x + (barWidth*i) + (barWidth/2), 0]; 
         return "translate("+ translate +")";
     });
